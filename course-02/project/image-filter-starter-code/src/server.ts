ec54d87c893fe2b5364f49a9d95679fbff41f911
hunglh2 <luonghonghung95@gmail.com>
2023-06-11 12:56:12 +0700

    fix the typescript typing for the req, res parameters and image_url

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,8 +29,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get( "/filteredimage", async ( req, res ) => {
-    const {image_url}: any = req.query;
+  app.get( "/filteredimage", async ( req: Request, res: Response) => {
+    const image_url: string = req.query.image_url as string;
     if( !image_url ) {
       res.status(400)
         .send(`image_url is required`);
