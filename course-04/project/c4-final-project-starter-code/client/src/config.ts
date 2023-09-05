a6b526cf9d4324efbcdf7ee4ecef8c08321693e8
honghungluong <luonghonghung95@gmail.com>
2023-09-05 13:23:37 +0700

    complete test fix bug

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-// https://46hc3wg3b2.execute-api.us-east-1.amazonaws.com/dev/todos
-const apiId = '46hc3wg3b2'
+// https://yh4be8hmk4.execute-api.us-east-1.amazonaws.com/dev/todos
+const apiId = 'yh4be8hmk4'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
